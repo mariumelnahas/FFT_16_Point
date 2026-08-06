1e9b6d85dcaf447e666c5c0fa8eb75abb0ecf316
Marium Waleed <marium.elnahas@gmail.com>
2026-08-06 18:56:50 +0300

    clk_div  module

---
+++ RTL/Clk_div.sv
@@ -0,0 +1,14 @@
+module clk_div (
+    input  wire clk_in,
+    input  wire rst,      // async, active-high
+    output reg  clk_out
+);
+ 
+    always @(posedge clk_in or posedge rst) begin
+        if (rst)
+            clk_out <= 1'b0;
+        else
+            clk_out <= ~clk_out;
+    end
+ 
+endmodule
